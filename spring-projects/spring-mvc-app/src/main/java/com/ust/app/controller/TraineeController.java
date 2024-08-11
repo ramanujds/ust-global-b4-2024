680dcb6d5c095a7023d42cf9e612ed623a4cbcd0
Ramanuj Das <ramanujds9@gmail.com>
2024-08-11 12:59:57 +0530

    Updated with Reading Form Data and Saving data to Repository

--- spring-projects/spring-mvc-app/src/main/java/com/ust/app/controller/TraineeController.java
+++ spring-projects/spring-mvc-app/src/main/java/com/ust/app/controller/TraineeController.java
@@ -1,14 +1,20 @@
 package com.ust.app.controller;
 
 import com.ust.app.model.Trainee;
+import com.ust.app.repository.TraineeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TraineeController {
+
+    @Autowired
+    private TraineeRepository traineeRepo;
 
 //    @PostMapping("/add-trainee")
 //    public String addTrainee(@RequestParam("id") int id,
@@ -22,6 +28,14 @@ public class TraineeController {
 
     @PostMapping("/add-trainee")
     public String addTrainee(@ModelAttribute("trainee") Trainee trainee){
+        traineeRepo.addTrainee(trainee);
+        return "show-info.jsp";
+    }
+
+    @GetMapping("/find-trainee")
+    public String getTrainee(@RequestParam("id") int id, Model m){
+        Trainee trainee = traineeRepo.getTraineeById(id);
+        m.addAttribute("trainee",trainee);
         return "show-info.jsp";
     }
 
---
+++ spring-projects/spring-mvc-app/src/main/java/com/ust/app/repository/TraineeRepository.java
@@ -0,0 +1,31 @@
+package com.ust.app.repository;
+
+
+import com.ust.app.model.Trainee;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class TraineeRepository {
+
+    private List<Trainee> trainees;
+
+    public TraineeRepository(){
+        this.trainees=new ArrayList<>();
+    }
+
+    public Trainee addTrainee(Trainee trainee){
+        trainees.add(trainee);
+        return trainee;
+    }
+
+    public Trainee getTraineeById(int id){
+       return trainees.stream().filter(t->t.getId()==id).findFirst().get();
+    }
+
+    public List<Trainee> getAll(){
+        return trainees;
+    }
+
+}
